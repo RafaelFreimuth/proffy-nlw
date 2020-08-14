080a960560063bd2a4443262726e527f31ed8244
RafaelFreimuth <rafaelfreimuth@outlook.com>
2020-08-14 16:56:27 -0300

    organizando codigo

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -63,7 +63,6 @@ function TeacherForm() {
 
     return (
 
-
         <div id="page-teacher-form" className="container">
             <PageHeader 
                 title="Que incrível que você quer dar aulas"
